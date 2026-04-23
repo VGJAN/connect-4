f69ff6988a047f2cb77871ea369704ee27d455a8
Gowsaljan <64354172+VGJAN@users.noreply.github.com>
2026-04-23 05:29:02 +0100

    feat: add win detection logic

--- js/app.js
+++ js/app.js
@@ -19,11 +19,55 @@ for (let i = 0; i < 6; i++) {
     const cell = row.insertCell();
     cell.setAttribute("data-row", i);
     cell.setAttribute("data-col", j);
+    cell.id = `${i}-${j}`;
     cell.className = "empty";
   }
 }
 
 gameContainer.appendChild(gameTable);
+
+const possibleWins = [];
+
+// rows
+for (let i = 0; i < 6; i++) {
+  for (let j = 0; j < 4; j++) {
+    let combo = [];
+    for (let k = 0; k < 4; k++) {
+      combo.push(`${i}-${j + k}`);
+    }
+    possibleWins.push(combo);
+  }
+}
+// columns
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 7; j++) {
+    let combo = [];
+    for (let k = 0; k < 4; k++) {
+      combo.push(`${i + k}-${j}`);
+    }
+    possibleWins.push(combo);
+  }
+}
+// forward diagonals
+for (let i = 3; i < 6; i++) {
+  for (let j = 0; j < 4; j++) {
+    let combo = [];
+    for (let k = 0; k < 4; k++) {
+      combo.push(`${i - k}-${j + k}`);
+    }
+    possibleWins.push(combo);
+  }
+}
+// backward diagonals
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 4; j++) {
+    let combo = [];
+    for (let k = 0; k < 4; k++) {
+      combo.push(`${i + k}-${j + k}`);
+    }
+    possibleWins.push(combo);
+  }
+}
 
 const activeDisc = createIcon("fa-circle");
 activeDisc.id = "activeDisc";
@@ -35,7 +79,7 @@ floatDisc.id = "floatDisc";
 floatDisc.classList.add("transparent");
 gameContainer.appendChild(floatDisc);
 
-updateFloatDisc()
+updateFloatDisc();
 
 function createIcon(iconName) {
   const icon = document.createElement("i");
@@ -56,10 +100,10 @@ function updateFloatDisc() {
 function updateActiveDisc() {
   const col = this.dataset.col;
   const headerCells = document.querySelectorAll("#gameTable thead th");
-  
+
   headerCells.forEach((cell) => (cell.innerHTML = ""));
   headerCells[col].appendChild(activeDisc);
-  
+
   floatDisc.classList.remove("transparent", "red", "yellow");
   floatDisc.classList.add(currentPlayer);
 
@@ -68,17 +112,41 @@ function updateActiveDisc() {
 
 function dropDisc() {
   const col = this.dataset.col;
-  for (let row = tbody.rows.length-1; row >= 0; row--) {
+  for (let row = tbody.rows.length - 1; row >= 0; row--) {
     const cell = tbody.rows[row].cells[col];
     if (!cell.innerHTML) {
       cell.className = currentPlayer;
       cell.appendChild(createIcon("fa-circle"));
       cell.setAttribute("style", "mask: unset");
 
-      currentPlayer = currentPlayer === "red" ? "yellow" : "red";
-      updateActiveDisc.call(this);
-      console.log(cell);
+      if (!checkWin()) {
+        currentPlayer = currentPlayer === "red" ? "yellow" : "red";
+        updateActiveDisc.call(this);
+      } else {
+        gameContainer.classList.add("gameOver");
+        console.log("-------------- " + currentPlayer + " won --------------");
+      }
       break;
+    }
+  }
+}
+
+function checkWin() {
+  for (var i = 0; i < possibleWins.length; i++) {
+    const cell1 = document.getElementById(possibleWins[i][0]);
+    const cell2 = document.getElementById(possibleWins[i][1]);
+    const cell3 = document.getElementById(possibleWins[i][2]);
+    const cell4 = document.getElementById(possibleWins[i][3]);
+
+    const cellClassCheck = [
+      cell1.className === currentPlayer,
+      cell2.className === currentPlayer,
+      cell3.className === currentPlayer,
+      cell4.className === currentPlayer,
+    ];
+
+    if (!cellClassCheck.includes(false)) {
+      return true;
     }
   }
 }
