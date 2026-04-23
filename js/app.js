5bdae3e4eec310200ec6851d25ea203eb0f0fe63
Gowsaljan <64354172+VGJAN@users.noreply.github.com>
2026-04-23 01:22:34 +0100

    feat: implement hover tracking

--- js/app.js
+++ js/app.js
@@ -24,3 +24,44 @@ for (let i = 0; i < 6; i++) {
 }
 
 gameContainer.appendChild(gameTable);
+
+const activeDisc = createIcon("fa-circle");
+activeDisc.id = "activeDisc";
+activeDisc.classList.add("transparent");
+headerRow.cells[0].appendChild(activeDisc);
+
+const floatDisc = createIcon("fa-circle");
+floatDisc.id = "floatDisc";
+gameContainer.appendChild(floatDisc);
+
+updateFloatDisc()
+
+function createIcon(iconName) {
+  const icon = document.createElement("i");
+  icon.classList.add("fa-solid", iconName);
+  return icon;
+}
+
+function updateFloatDisc() {
+  const activeRect = activeDisc.getBoundingClientRect();
+  const containerRect = gameContainer.getBoundingClientRect();
+  const top = activeRect.top + activeRect.height / 2 - containerRect.top;
+  const left = activeRect.left + activeRect.width / 2 - containerRect.left;
+
+  floatDisc.style.top = top + "px";
+  floatDisc.style.left = left + "px";
+}
+
+function updateActiveDisc() {
+  const col = this.dataset.col;
+  const headerCells = document.querySelectorAll("#gameTable thead th");
+  
+  headerCells.forEach((cell) => (cell.innerHTML = ""));
+  headerCells[col].appendChild(activeDisc);
+
+  updateFloatDisc();
+}
+
+document.querySelectorAll("td").forEach((cell) => {
+  cell.addEventListener("mouseover", updateActiveDisc);
+});
